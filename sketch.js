96932d719ae3433847611aa7baded395592e3262
Qahraman <jockopolo666@gmail.com>
2024-08-17 19:43:00 +0500

    Mostly done

--- sketch.js
+++ sketch.js
@@ -1,1 +1,44 @@
-//Testing
+const container = document.querySelector("#container")
+const button = document.querySelector("button")
+
+
+
+let input = "10";
+button.addEventListener("click",()=> {
+   input = prompt("Number of squares per each side? must be less than 100:");
+  
+   if (input > 0 && input < 100){
+   GridGen(input)
+   }
+})
+GridGen(input)
+
+
+
+
+//Our function
+function GridGen (size){
+container.innerHTML = "";
+ 
+let WidthHeight = (800/size);
+console.log(WidthHeight)
+
+let num = 1;
+while (num <= size*size)
+{
+   const div = document.createElement("div")
+   container.appendChild(div)
+
+  
+
+   div.style.width = `${WidthHeight}px`
+   div.style.height = `${WidthHeight}px`
+
+   div.textContent = `div ${num}`
+
+   div.addEventListener("mouseenter", ()=> div.style.backgroundColor = "yellow")
+  // div.addEventListener("mouseleave", ()=> div.style.backgroundColor = "skyblue")
+   num++
+}
+}
+
